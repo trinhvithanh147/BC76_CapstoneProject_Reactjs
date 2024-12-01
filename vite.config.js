2eaef19e3f6d532f7026604593d5e5d48ed71d68
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2024-12-01 15:39:22 +0700

    gan xong

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   assetsInclude: ["**/*.png", "**/*.jpg", "**/*.svg", "**/*.webp"],
 });

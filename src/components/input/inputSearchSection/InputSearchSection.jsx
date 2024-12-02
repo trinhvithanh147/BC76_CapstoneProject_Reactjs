73184acf7dd1773e08c20f64ac78b44c85a25c9f
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2024-12-02 10:00:35 +0700

    vua responsive lai

--- src/components/input/inputSearchSection/InputSearchSection.jsx
+++ src/components/input/inputSearchSection/InputSearchSection.jsx
@@ -19,10 +19,10 @@ const InputSearchSection = ({
         value={value}
         onKeyDown={handleKeyDown}
         onSearch={handleSearch}
-        className="lg:w-[616px] lg:h-[60px] w-[328px] h-[52px] rounded-xl text-base !text-inherit !bg-white border border-solid !border-[#c5c6c9] font-bold leading-6"
+        className="lg:w-[616px] lg:h-[60px] md:w-[408px] md:h-[52px] w-[328px] h-[52px] rounded-xl text-base !text-inherit !bg-white border border-solid !border-[#c5c6c9] font-bold leading-6"
       />
       <Button
-        className="lg:!w-[48px]  lg:h-[48px] !w-[40px] h-[40px]  rounded-lg bg-[#003912] text-white"
+        className="lg:!w-[48px]  lg:h-[48px] !w-[40px] h-[40px] md:!w-[40px] md:h-[40px]   rounded-lg bg-[#003912] text-white"
         onClick={() => handleSearch(value)}
         icon={<SearchOutlined />}
       />
